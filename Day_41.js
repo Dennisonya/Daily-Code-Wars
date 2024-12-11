33838abc542ae63965a2f9e966a03b48e56c128e
Dennis onya <dennisonya3@gmail.com>
2024-12-11 10:10:13 +0200

    (8KYU)

--- Day_41.js
+++ Day_41.js
@@ -13,3 +13,12 @@ function count(string) {
         }
     return string.length === 0 ? {} : freqMap;
   }
+
+  //Take an array and remove every second element from the array. 
+  //Always keep the first element and start removing with the next element.
+  //(8KYU) Removing Elements
+
+  function removeEveryOther(arr){
+
+    return arr.filter((_,index) => index % 2 === 0);
+  }
